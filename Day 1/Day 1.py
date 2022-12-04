cfa684c42a706a61ccb652f90e8f8dbc39387682
mfranzreb <119127691+mfranzreb@users.noreply.github.com>
2022-12-04 19:26:29 +0100

    Add Day 1

---
+++ Day 1/Day 1.py
@@ -0,0 +1,20 @@
+with open("input.txt", "r") as f:
+    content = [line.split("\n")[0] for line in f.readlines()]
+    calories = 0
+    info = {}
+    i = 1
+    max_calories = 0
+    for num in content:
+        
+        if not num:
+            info[i] = calories
+            if calories > max_calories:
+                max_calories = calories
+                max_calories_index = i
+            calories = 0
+            i+=1
+            continue
+        calories =+ int(num)
+
+    print("Elf {index} is carrying the most calories, which are {cals}".format(index = max_calories_index, cals = info[max_calories_index]))
+
