e0a5e6e887741df38e337a114e70493d1fde7a75
mfranzreb <119127691+mfranzreb@users.noreply.github.com>
2022-12-04 20:17:27 +0100

    Change for second problem

--- Day 1/Day 1.py
+++ Day 1/Day 1.py
@@ -1,23 +1,20 @@
 with open("input.txt", "r") as f:
     content = [line.split("\n")[0] for line in f.readlines()]
     calories = 0
-    info = {}
-    i = 1
-    max_calories = 0
+    info = []
+
     for num in content:
-        
         if not num:
-            print("____________________")
-            info[i] = calories
-            if calories > max_calories:
-                max_calories = calories
-                max_calories_index = i
+            info.append(calories)
             calories = 0
-            i+=1
         else:
             calories += int(num)
-            print(int(num), calories)
+    info.sort(reverse=True)
 
-    print("Elf {index} is carrying the most calories, which are {cals}".format(index = max_calories_index, cals = max_calories))
+    f.close()
+
+print("Max calories on one elf: ", info[0])
+print("Calories on top three elves: ", sum(info[:3]))
+
 
 
