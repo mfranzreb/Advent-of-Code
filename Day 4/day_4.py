508438a002da88c6174349141a721d299dad8f48
mfranzreb <119127691+mfranzreb@users.noreply.github.com>
2022-12-06 10:04:54 +0100

    Add day 4 part 1

---
+++ Day 4/day_4.py
@@ -0,0 +1,13 @@
+with open("input.txt", "r") as f:
+    content = [line.strip("\n").split(",") for line in f.readlines()]
+
+    count = 0
+    for line in content:
+        first_pair = line[0].split("-")
+        second_pair = line[1].split("-")
+        if int(first_pair[0]) >= int(second_pair[0]) and int(first_pair[1]) <= int(second_pair[1]) or int(first_pair[0]) <= int(second_pair[0]) and int(first_pair[1]) >= int(second_pair[1]):
+            count +=1
+
+    print(count)
+
+
