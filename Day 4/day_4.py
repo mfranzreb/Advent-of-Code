ad032fbbeadbfdb1c349c6561f87bd1ef7a3f455
mfranzreb <119127691+mfranzreb@users.noreply.github.com>
2022-12-06 10:30:43 +0100

    Add day 4 part 2

--- Day 4/day_4.py
+++ Day 4/day_4.py
@@ -9,5 +9,15 @@ with open("input.txt", "r") as f:
             count +=1
 
     print(count)
+#Part 2___________________
 
-
+    count = 0
+    for line in content:
+        first_pair = line[0].split("-")
+        second_pair = line[1].split("-")
+        if not (int(first_pair[1]) < int(second_pair[0]) or int(first_pair[0]) > int(second_pair[1])):
+            count += 1
+            
+        
+    print(count)
+    f.close()
