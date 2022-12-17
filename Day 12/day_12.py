0e6f5c91f892adb4792dbd75d9ac465a701f7678
mfranzreb <119127691+mfranzreb@users.noreply.github.com>
2022-12-17 10:49:16 +0100

    improve speed from 32 sec to 28 sec

--- Day 12/day_12.py
+++ Day 12/day_12.py
@@ -1,4 +1,7 @@
 from copy import copy
+from timeit import default_timer as timer
+
+beg = timer()
 
 
 def read_input():
@@ -18,59 +21,35 @@ def main():
     coords = get_coords(graph)
     start_coord, end_coord = coords[0], coords[1]
     graph[end_coord[1]][end_coord[0]] = ord("z") - 96
+    graph[start_coord[1]][start_coord[0]] = ord("a") - 96
 
     
 
     path = shortest_path(graph, start_coord, end_coord)
 
-    print(path)
+    #print(path)
     print(len(path) - 1)
 
 
 def shortest_path(graph, start_coord, end_coord):
-    paths = []
-    paths.append([start_coord])
-    map_width = len(graph[0]) -1 
-    map_height = len(graph) -1
+    paths = [[start_coord]]
     #i = 1
-    
+    directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
     while True:
         new_paths = []
         for path in paths:
             current_coord = path[-1]
-            possible_directions = []
-            if len(path) == 1:
-                possible_directions.append((1, 0))
-                possible_directions.append((0, -1))
-                possible_directions.append((0, 1))
+            new_steps = get_possible_directions(directions, paths, new_paths, graph, current_coord)
+            for step in new_steps:
 
-            else:
-                if current_coord[0] != 0 and current_coord[0] - 1 != path[-2][0] and (graph[current_coord[1]][current_coord[0] - 1] - 1 <=  graph[current_coord[1]][current_coord[0]]): #Y coordinate goes first!!
-                    possible_directions.append((-1, 0))
-
-                if current_coord[0] != map_width and current_coord[0] + 1 != path[-2][0] and graph[current_coord[1]][current_coord[0] + 1] - 1 <=  graph[current_coord[1]][current_coord[0]]:
-                    possible_directions.append((1, 0))
-
-                if current_coord[1] != 0 and current_coord[1] - 1 != path[-2][1] and graph[current_coord[1] - 1][current_coord[0]] - 1 <=  graph[current_coord[1]][current_coord[0]]:#en y, sumar uno en indice es ir hacia baajo
-                    possible_directions.append((0, -1))
-
-                if current_coord[1] != map_height and current_coord[1] + 1 != path[-2][1] and graph[current_coord[1] + 1][current_coord[0]] - 1 <=  graph[current_coord[1]][current_coord[0]]:
-                        possible_directions.append((0, 1))
-
-            for dir in possible_directions:
-                new_step = tuple(map(lambda i, j: i + j, current_coord, dir))
-
-                if new_step == end_coord:
+                if step == end_coord:
                     new_path = copy(path)
-                    new_path.append(new_step)
+                    new_path.append(step)
                     return new_path
-
-                elif any(new_step in p for p in paths) or any(new_step in t for t in new_paths):
-                    continue
 
                 else:
                     new_path = copy(path)
-                    new_path.append(new_step)
+                    new_path.append(step)
                     new_paths.append(new_path)
         """if i % 5== 0 and i > 322:
             vis_map = [["_" for i in range(map_width+1)] for j in range(map_height+1)] # visualization
@@ -92,14 +71,28 @@ def shortest_path(graph, start_coord, end_coord):
             paths.append(new_paths)
             paths = paths[0]
         #i+=1  
+
+def get_possible_directions(directions, paths, new_paths, graph, current_coord):
+    map_width = len(graph[0]) -1 
+    map_height = len(graph) -1
+    new_steps = []
+    for dir in directions:
+        new_step = [current_coord[0] + dir[0], current_coord[1] + dir[1]]
+        if not (any(new_step in p for p in paths) or any(new_step in t for t in new_paths)) and not (new_step[0] < 0 or new_step[0] > map_width) and not (new_step[1] < 0 or new_step[1] > map_height) and graph[new_step[1]][new_step[0]] - 1 <= graph[current_coord[1]][current_coord[0]]:
+            new_steps.append(new_step)
+
+    return new_steps
+
+
+
                 
 def get_coords(graph):
     for y, line in enumerate(graph):
         for x, num in enumerate(line):
             if num == -13:
-                start_coord = (x, y)
+                start_coord = [x, y]
             elif num == -27:
-                end_coord = (x, y)
+                end_coord = [x, y]
 
     return start_coord, end_coord
         
@@ -108,3 +101,6 @@ def get_coords(graph):
 
 if __name__ == "__main__":
     main()
+
+end = timer()
+print(end - beg)
